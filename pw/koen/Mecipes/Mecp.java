3a733e3f7c33c6d00222b4146d9ac59767a23bde
Matty Lee <themtz@users.noreply.github.com>
2014-02-21 12:34:05 +0900

    Removed Import Listener

--- pw/koen/Mecipes/Mecp.java
+++ pw/koen/Mecipes/Mecp.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
